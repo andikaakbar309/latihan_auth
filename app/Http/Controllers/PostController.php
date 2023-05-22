2ef45b96a8ba4bf21f3ba28fb7620b5f8cca8d42
andikaakbar309 <andikaakskyt67@gmail.com>
2023-05-22 14:47:38 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+
+    public function index(){
+
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create(){
+        $categories = Category::all();
+        return view('posts.create', compact('categories'));
+    }
+
+    public function store(Request $request){
+        
+        $author_id = Auth()->user()->id;
+        // dd($author_id);
+
+        Post::create([
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+            'body' => $request->body,
+            'category_id' => $request->category_id,
+            'author_id' => $author_id
+        ]);
+        return redirect()->back();
+    }
+
+    public function edit($id){
+        $post = Post::whereId($id)->first();
+        $categories = Category::all();
+        return view('posts.edit', compact('categories', 'post'));
+    }
+}
---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.dashboard')
+
+@push('style')
+<!-- Custom styles for this page -->
+<link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+@endpush
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800 py-4">Category</h1>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-body">
+
+            <!-- <button type="button" class="btn btn-primary mb-3 px-4" data-toggle="modal" data-target="#modal_add_category"><i class="fa fa-plus"></i>
+                Create Category
+            </button> -->
+
+            <a href="/dashboard/category/create" class="btn btn-primary mb-3">Add Category</a>
+
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Slug</th>
+                            <th>Posts</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                        $no=1;
+                        @endphp
+                        @foreach($categories as $category)
+                        <tr>
+                            <td>{{ $no++ }}</td>
+                            <td>{{ $category->name }}</td>
+                            <td>{{ $category->slug }}</td>
+                            <td>{{ $category->posts->count() }}</td>
+                            <td>
+                                <div class="dropdown">
+                                    <i class="fa fa-ellipsis-v" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="/dashboard/category/edit/{{ $category->id }}"><i class="fas fa-edit pr-2 text-primary"></i>Edit</a>
+                                        <a class="dropdown-item" href="/dashboard/category/delete/{{ $category->id }}" onclick="return confirm('Sure??, Your data will lost')">
+                                            <i class="fas fa-trash-alt pr-2 text-danger"></i>Delete
+                                        </a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modal_add_category" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Category</h1>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('script')
+<!-- Page level plugins -->
+<script src="/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="/js/demo/datatables-demo.js"></script>
+@endpush
